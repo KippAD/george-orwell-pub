a3378f55ebac8e5db128cebe57033a75989d7805
Kipp AD <kippad@protonmail.com>
2022-10-05 19:31:40 +0000

    Add events class model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import user
+from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+# Model for events
+class Events(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    image = CloudinaryField('image', default='placeholder')
+    description = models.TextField(blank=True)
+    time = models.TimeField()
+    date = models.DateTimeField()
+    price = models.IntegerField()
+    repeating = models.BooleanField(default=False)
+    capacity = models.IntegerField(
+        validators=[
+            MaxValueValidator(64),
+            MinValueValidator(1)
+        ]
+    )
+
+    def __str__(self):
+        return self.title
+
+
