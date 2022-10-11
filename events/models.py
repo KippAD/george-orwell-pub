b972b04c6c5ca827c74d1f681c45b8f31081345a
Kipp AD <kippad@protonmail.com>
2022-10-11 13:23:14 +0000

    Create superuser and admin panel

--- events/models.py
+++ events/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -8,19 +8,19 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Events(models.Model):
     title = models.CharField(max_length=200, unique=True)
     image = CloudinaryField('image', default='placeholder')
-    description = models.TextField(blank=True)
-    time = models.TimeField()
-    date = models.DateTimeField()
-    price = models.IntegerField()
-    repeating = models.BooleanField(default=False)
-    capacity = models.IntegerField(
-        validators=[
-            MaxValueValidator(64),
-            MinValueValidator(1)
-        ]
-    )
+    # description = models.TextField(blank=True)
+    # time = models.TimeField()
+    # date = models.DateTimeField()
+    # price = models.IntegerField()
+    # repeating = models.BooleanField(default=False)
+    # capacity = models.IntegerField(
+    #     validators=[
+    #         MaxValueValidator(64),
+    #         MinValueValidator(1)
+    #     ]
+    # )
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
 
 
