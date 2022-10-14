6a866fbfe9237baf52bf7bf5b177041a7b7a2919
Kipp AD <kippad@protonmail.com>
2022-10-14 18:11:26 +0000

    Create view for site home page

--- events/views.py
+++ events/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Event
 
-# Create your views here.
+
+class HomePage(View):
+    """
+    Renders the home page index.html
+    """
+    def get(self, request):
+        return render(request, "index.html")
--- orwellpub/urls.py
+++ orwellpub/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from events import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', views.HomePage.as_view(), name='home')
 ]
